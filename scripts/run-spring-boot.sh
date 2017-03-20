da577388d6e254ef790f56f01cdc6bedfea28746
Bruno Simioni <brunosimioni@gmail.com>
2017-03-19 22:55:47 -0300

    working fine in docker-compose

--- scripts/run-spring-boot.sh
+++ scripts/run-spring-boot.sh
@@ -5,14 +5,9 @@ export JAR_MODULE=/app/$2
 export NEWRELIC_CONFIGFILE=$3
 export IP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 export NEWRELIC_ARGS="-javaagent:/newrelic/newrelic.jar -Dnewrelic.config.file=/newrelic/config/$NEWRELIC_CONFIGFILE"
-export MEM_ARGS="-Xmx$1m -Xss$1m -Xint"
-export JMX_ARGS="-Dcom.sun.management.jmxremote.rmi.port=3333 \
-    -Dcom.sun.management.jmxremote=true \
-    -Dcom.sun.management.jmxremote.port=3333 \
-    -Dcom.sun.management.jmxremote.ssl=false \
-    -Dcom.sun.management.jmxremote.authenticate=false \
-    -Dcom.sun.management.jmxremote.local.only=false \
-    -Djava.rmi.server.hostname=$IP"
+export MEM_ARGS="-Xmx$1m -Xss$1m"
+export INTERPRETED_MODE="-Xint"
+export JVM_ARGS="-server -XX:+TieredCompilation -XX:TieredStopAtLevel=1"
 
-echo "Executing java -jar $NEWRELIC_ARGS $MEM_ARGS $JMX_ARGS $MEM_SIZE $JAR_MODULE"
-java -jar $NEWRELIC_ARGS $MEM_ARGS $JMX_ARGS $JAR_MODULE
+echo "Executing java -jar $NEWRELIC_ARGS $MEM_ARGS $JVM_ARGS $JAR_MODULE"
+java -jar $NEWRELIC_ARGS $MEM_ARGS $JVM_ARGS $JAR_MODULE
--- scripts/wait-for-config-and-discovery.sh
+++ scripts/wait-for-config-and-discovery.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # run inside docker container
-export DISCOVERY_URL=ds-toolbox-discovery-service:8080
-export CONFIG_URL=ds-toolbox-config-service:8080
+export DISCOVERY_URL=discovery-service:8080
+export CONFIG_URL=config-service:8080
 export CMD=${@:1}
-/scripts/wait-for-it.sh -t 60 -s $DISCOVERY_URL -- /scripts/wait-for-it.sh -t 60 -s $CONFIG_URL -- $CMD
+/scripts/wait-for-it.sh -t 120 -s $DISCOVERY_URL -- /scripts/wait-for-it.sh -t 120 -s $CONFIG_URL -- $CMD
